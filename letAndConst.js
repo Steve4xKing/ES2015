22f5f5649bc7032d51866b81406dce355bfa0a7d
Steve4xKing <stevenaking1996@gmail.com>
2024-02-19 18:16:42 -0500

    js file saved this time

---
+++ letAndConst.js
@@ -0,0 +1,10 @@
+const PI = 3.14;
+
+
+// var and let are similar, but you can not redeclare let. and Let is block scope.
+
+// var and const differ because you can not reassign or redeclare const. Const is also block scope
+
+// you can reassign let but you can not reassign const
+
+// hoisting is when varaibles are initialized at the beginning of the code being run and can be accesses before it is defined.  
